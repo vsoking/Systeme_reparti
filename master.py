90cb5e5c6140af7a2e01a6dc91fab9967bc0e8e5
vsoking <>
2021-10-21 04:53:16 +0200

    etape 11 completed

--- master.py
+++ master.py
@@ -1,19 +1,19 @@
 import subprocess
 from sys import stderr, stdin
 from threading import Thread
-
-usr_name = "vsoking-20"
-machine_prefix = "tp-4b01-"
-machine_list = ['39', '40']#, '41', '42']
-sleep_time_list = ['10', '10']
-timeout_list = [2, 15]
+import warnings
+import time
+import ast
+import json
+machine_list = ["vsoking-20@tp-4b01-"+"%02d" % i for i in range(39,42)]
+#sleep_time_list = ["%02d" % 1 for i in range(39,42)]
+#timeout_list = [3 for i in range(39,41)]
 exec = "slave.py"
-remote_dir_path = "/tmp/" + usr_name + "/"
+remote_dir_path = "/tmp/vsoking-20"
 process_list = []
 thread_list = []
+script_name = "/tmp/vsoking-20/slave.py" 
 
-def usr_hostname(n):
-    return usr_name + '@' + machine_prefix + n
 
 def run_func(ps):
     out, err = ps.communicate()
@@ -21,13 +21,169 @@ def run_func(ps):
     print("out: ", out)
     print("err:", err)
 
-# create directory and copy executable
-for machine in machine_list:
-    subprocess.call(['ssh', usr_hostname(machine), "mkdir -p ", remote_dir_path], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-    subprocess.call(['scp', exec, usr_hostname(machine)+":"+remote_dir_path], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+def create_dir(dir_name, slaves):
+    proc_list = []
+    r = 0
+    for s in slaves:
+        p = subprocess.Popen(['ssh', s, "mkdir -p ", dir_name], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc_list.append(p)
+    for p, s in zip(proc_list, slaves):
+        out , err = p.communicate()
+        if p.returncode:
+            raise RuntimeError("'{} '{} error '{} to create dir: '{}".format(out, err, p.returncode, s))   
+
+
+def deploy(slaves_file, dir):
+    proc_list = []
+    r = 0
+    for s_f in slaves_file:
+        p = subprocess.Popen(['scp',  s_f[1], s_f[0]+":"+dir], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc_list.append(p)
+    for p, s in zip(proc_list, slaves_file):
+        out , err = p.communicate()
+        if p.returncode:
+            raise RuntimeError("'{} '{} error '{} unable to deploy on: '{}".format(out, err, p.returncode, s_f[0]))
+
+def execute_map(slaves_files):
+       
+    proc_list = []
+    r = 0
+    for s_f in slaves_files:
+        cmd = "python3 {} -m {} {}".format(script_name, s_f[1], 'UM'+s_f[1][1:])
+        p = subprocess.Popen(['ssh',  s_f[0], cmd], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc_list.append(p)
+    for p, s in zip(proc_list, slaves_files):
+        out , err = p.communicate()
+        if p.returncode:
+            raise RuntimeError("'{} '{} error '{} map error on: '{}".format(out, err, p.returncode, s_f[0]))
+
+def execute_shuffle(slaves_files):
+    proc_list = []
+    r = 0
+    for s_f in slaves_files:
+        cmd = "python3 {} -s {} {}".format(script_name, 'UM'+s_f[1][1:], 'To_avoid_err')
+        p = subprocess.Popen(['ssh',  s_f[0], cmd], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc_list.append(p)
+    for p, s in zip(proc_list, slaves_files):
+        out , err = p.communicate()
+        if p.returncode:
+            raise RuntimeError("'{} '{} error '{} shuffle error on: '{}".format(out, err, p.returncode, s_f[0])) 
+
+def execute_reduce(slaves):
+    proc_list = []
+    r = {}
+    for s in slaves:
+        cmd = "python3 {} -r {} {}".format(script_name, 'To_avoid_err', 'To_avoid_err')
+        p = subprocess.Popen(['ssh',  s, cmd], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc_list.append(p)
+    for p, s in zip(proc_list, slaves):
+        out , err = p.communicate()
+        out = out.decode("utf-8")
+        if p.returncode:
+            raise RuntimeError("'{} '{} error '{} reduce error on: '{}".format(out, err, p.returncode, s))
+        elif len(out):            
+            out = ast.literal_eval(out)
+            r.update(out)
+    return r
 
 
 
+def get_connected_slaves(slaves):
+    proc_list = []
+    slaves_ok = []
+    for s in slaves:
+        p = subprocess.Popen(['ssh', s, "hostname"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc_list.append(p)
+
+    for s, p in zip(slaves, proc_list):
+        p.communicate()
+        if not p.returncode:
+            slaves_ok.append(s)
+    return slaves_ok
+
+ 
+      
+
+def clean(slaves):
+    proc_list = []
+    r = 0    
+    for s in slaves:
+        p = subprocess.Popen(['ssh', s, "rm -rf ", remote_dir_path], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc_list.append(p)
+    for p, s in zip(proc_list, slaves):
+        p.communicate()
+        if p.returncode:
+            warnings.warn("error '{} unable to clean: '{}".format(p.returncode, s))
+        
+
+
+def main():
+
+    m = get_connected_slaves(machine_list)
+
+    create_dir(remote_dir_path, m)
+    split_dir = remote_dir_path+"/splits"
+    create_dir(split_dir, m)
+    #map_dir = remote_dir_path+"/maps"
+    #create_dir(map_dir, m)
+
+    d_list = [(s, "slave.py") for s in m]
+    deploy(d_list, remote_dir_path)
+    
+    
+
+    #create files
+    s0 = open("S0.txt",'w')
+    for i in range(10):
+        s0.write("Deer Beer River")
+    
+    s1 = open("S1.txt",'w')
+    for i in range(1):
+        s1.write("Car Car River")
+    
+    s2 = open("S2.txt", 'w')
+    for i in range(1):
+        s2.write("Deer Car Beer")
+
+    s0.close()
+    s1.close()
+    s2.close()
+
+    #create list of all files name
+    file_list = ["S{}.txt".format(i) for i in range(0,3)]
+
+    #create list of (slave_name, file_to_deploy, directory where file will be deploy)
+    
+    d_list = [(s, f) for s, f  in zip(m, file_list)]
+
+    #copy files to remote machine
+    deploy(d_list, split_dir)
+
+    start_time = time.perf_counter()
+    execute_map(d_list)
+    print("MAP FINISHED... {:.2f} seconds".format(time.perf_counter() - start_time) )
+
+    m_file = open("machines.txt", 'w')
+    m_file.write(" ".join(m))
+    m_file.close()
+
+    start_time = time.perf_counter()
+    deploy([(s, "machines.txt") for s in m], remote_dir_path)
+    execute_shuffle(d_list)
+    print("SHUFFLE FINISHED... {:.2f} seconds".format(time.perf_counter() - start_time))
+
+    start_time = time.perf_counter()
+    r = execute_reduce(m)
+    print("REDUCE FINISHED... {:.2f} seconds".format(time.perf_counter() - start_time))
+    print("RESULTS: \n{}".format(r))
+
+    clean(m)
+
+
+if __name__ == "__main__":
+    main()
+
+"""
 for machine, t in zip(machine_list, sleep_time_list):
     proc = subprocess.Popen(['ssh', usr_hostname(machine), "python", remote_dir_path+exec, str(t) ], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
     thrd = Thread(target=run_func, args=(proc,), daemon=True)
@@ -37,8 +193,13 @@ for machine, t in zip(machine_list, sleep_time_list):
 
 
 for thrd, t , p in zip(thread_list, timeout_list, process_list):
-    thrd.join(t)
+    thrd.join()
     if thrd.is_alive():
         print("Timeout... Killing the process")
         p.kill()
+
+#clean
+for machine in machine_list:
+    subprocess.call(['ssh', usr_hostname(machine), "rm -rf ", remote_dir_path], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
         
+"""
--- slave.py
+++ slave.py
@@ -1,10 +1,128 @@
-import time
-import sys
 
-def main(argv):
-    time.sleep(int(argv[0]))
-    #print("slave slept for: %d and  calculate 3+5= %d" %(int(argv[0]),3+5))
-    sys.stderr.write("slave slept for: %d and  calculate 3+5= %d" %(int(argv[0]),3+5))
+import sys
+import argparse
+import subprocess
+import platform
+import hashlib
+import os
+
+host_name = platform.node()
+shuffle_received_dir_name = "shufflesreceived"
+work_dir_path = "/tmp/vsoking-20/"
+map_dir_name = "maps"
+
+def create_local_dir(dir_name):
+    r = 0
+    p = subprocess.Popen(["mkdir",'-p', dir_name], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    out , err = p.communicate()
+    if p.returncode:
+        raise RuntimeError("Failed to create {} directory".format(dir_name))
+
+def create_remote_dir(slaves, dir_name):
+    proc_list = []
+    r = 0
+    for s in slaves:
+        p = subprocess.Popen(['ssh', s, "mkdir -p ", dir_name], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc_list.append(p)
+    for p, s in zip(proc_list, slaves):
+        out , err = p.communicate()
+        if p.returncode:
+            raise RuntimeError("'{} '{} error '{} to create dir: '{}".format(out, err, p.returncode, s))
+
+def deploy(slaves_file, dir):
+    proc_list = []
+    r = 0
+    for s_f in slaves_file:
+        p = subprocess.Popen(['scp',  s_f[1], s_f[0]+":"+dir], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc_list.append(p)
+    for p, s in zip(proc_list, slaves_file):
+        out , err = p.communicate()
+        if p.returncode:
+            raise RuntimeError("'{} '{} error '{} unable to deploy on: '{}".format(out, err, p.returncode, s_f[0]))
+
+
+def map(input_file, output_file):
+    in_f = open("/tmp/vsoking-20/splits/"+input_file, 'r')
+    #create_local_dir("/tmp/vsoking-20/maps/")
+    os.makedirs("/tmp/vsoking-20/maps/", exist_ok=True)
+    out_f = open("/tmp/vsoking-20/maps/"+output_file, 'w')
+    s = in_f.read()
+    d = {}
+    for w in s.split():
+        out_f.write("{} 1\n".format(w))
+    in_f.close()
+    out_f.close()
+
+def shuffle(input_file):
+    shuffle_dir = "/tmp/vsoking-20/shuffles"
+    os.makedirs(shuffle_dir, exist_ok=True)
+    #create_local_dir(shuffle_dir)
+    in_f = open(work_dir_path+"maps/"+input_file, 'r')
+    m_f = open("/tmp/vsoking-20/machines.txt")
+    m_list = m_f.read().split(" ")
+    #host_id = m_list.index("vsoking-20@"+host_name)
+    hash_files_dict = {}
+    slaves_files_list = []    
+    for line in in_f:
+        s = bytes(line.split(" ")[0], "utf-8")
+        hash_ = int.from_bytes(hashlib.md5(s).digest(), 'big')
+        out_file_name = str(hash_) +'-'+host_name + ".txt"
+        hash_files_dict[hash_] = out_file_name
+        with open(shuffle_dir+"/"+out_file_name, "a+") as f:
+            f.writelines(line)
+    for h, f in hash_files_dict.items():
+        dest = h % len(m_list)
+        #if dest != host_id:
+        slaves_files_list.append( (m_list[dest], shuffle_dir+"/"+hash_files_dict[h]) )    
+    create_remote_dir([s[0] for s in slaves_files_list], work_dir_path+shuffle_received_dir_name)
+    deploy(slaves_files_list, work_dir_path+shuffle_received_dir_name)
+    in_f.close()
+    m_f.close()
+
+def reduce():
+    count_dict = {}
+    for shuffle_file_name in os.listdir(work_dir_path+shuffle_received_dir_name):
+        reduce_file_name = shuffle_file_name.split('-')[0]
+        with open(work_dir_path+shuffle_received_dir_name+"/"+shuffle_file_name, "r") as f:
+            lines_list = f.readlines()
+            key = lines_list[0].split(" ")[0]
+            if key in count_dict:
+                count_dict[key] += len(lines_list)
+            else:
+                count_dict[key] = len(lines_list)
+    reduce_dir_path = "/tmp/vsoking-20/reduces"
+    os.makedirs(reduce_dir_path, exist_ok=True)
+    out_f = open(reduce_dir_path+"/result-"+host_name+".txt", 'w')
+    for words_count in count_dict.items():
+        out_f.write(words_count[0]+" "+str(words_count[1]) +'\n')
+    out_f.close()
+    return count_dict
+
+        
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument("-m", "--map", help="perfom map operation", action="store_true")
+    group.add_argument("-r", "--reduce", help="perfom reduce operation", action="store_true")
+    parser.add_argument("-s", "--shuffle", help="perform shuffle operation", action="store_true")
+    parser.add_argument("input", help="input file")
+    parser.add_argument("output", help="output file")
+    args = parser.parse_args()
+    if args.map:
+        map(args.input, args.output)
+    elif args.shuffle:
+        shuffle(args.input)
+    elif args.reduce:
+        #reduce()
+        isdir = os.path.isdir("/tmp/vsoking-20/shufflesreceived")
+        if isdir:
+            print(reduce())
+        else:
+            pass
+    else:
+        pass
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
